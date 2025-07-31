25752dfdfa561b248e53d41d3a4a3d38089903b0
Gene Dan <genedan@gmail.com>
2025-07-30 21:54:09 -0500

    FEAT: Work on Portfolio class.

--- lucifer/porfolio.py
+++ lucifer/porfolio.py
@@ -1,2 +1,26 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from datetime import date
+    from lucifer.policy import Policy
+
 class Portfolio:
-    ...
+    def __init__(self):
+        self.policy_list = []
+
+    def add_policy(self, policy: Policy):
+        self.policy_list += [policy]
+
+    @property
+    def written_premium(self) -> float:
+
+        return sum([policy.written_premium for policy in self.policy_list])
+
+    def earned_premium(
+            self,
+            as_of: date
+    ) -> float:
+
+        return sum([policy.earned_premium(as_of=as_of) for policy in self.policy_list])
