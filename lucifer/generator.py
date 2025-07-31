ae9475e7e022009052fa8eb76eb1631824f78a17
Gene Dan <genedan@gmail.com>
2025-07-30 21:53:43 -0500

    FEAT: Add code to write policies.

--- lucifer/generator.py
+++ lucifer/generator.py
@@ -3,19 +3,23 @@ from time import sleep
 import datetime
 from datetime import date
 
+from lucifer.policy import Policy
+from lucifer.porfolio import Portfolio
+
 from rich.progress import Progress
 
-from typing import TYPE_CHECKING
+from scipy.stats import poisson, uniform
 
-if TYPE_CHECKING:
-    from porfolio import Portfolio
+from typing import TYPE_CHECKING
 
 def write_portfolio(
     start_date: date,
     end_date: date,
-    n_policies: int
+    poisson_mean: int,
+    premium_mean: float
 ) -> Portfolio:
 
+    portfolio = Portfolio()
     progress =  Progress()
     progress.start()
     current_date: date = start_date
@@ -25,7 +29,15 @@ def write_portfolio(
 
     day_tracker = progress.add_task("[red]Simulating...", total=n_days)
     while current_date <= end_date:
+        # Write the policies for the day.
+        n_policies_written = poisson.rvs(mu=poisson_mean)
+        for i in range(n_policies_written):
+            new_policy = Policy(effective_date=current_date, written_premium=uniform.rvs(loc=500, scale=premium_mean))
+            portfolio.add_policy(new_policy)
+
         progress.update(day_tracker, advance=1)
         current_date += datetime.timedelta(days=1)
         sleep(.02)
     progress.stop()
+
+    return portfolio
